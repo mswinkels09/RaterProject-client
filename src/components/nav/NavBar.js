2ff62fc49314399d1fca218cc37c04591a18f7aa
mswinkels09 <mswinkels09@gmail.com>
2020-11-02 11:44:51 -0600

    setup register and login

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                Navigation link
+            </li>
+            <li className="navbar__item">
+                Navigation link
+            </li>
+            <li className="navbar__item">
+                Navigation link
+            </li>
+            {
+                (localStorage.getItem("gr_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("gr_token")
+                                props.history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
